a00877dc8882c40144d8ca1c0cb2bda2f0c00b68
Justina Kuchta <justyna.kuchta@gmail.com>
2019-08-11 23:23:31 +0000

    Automatically backed up by Learn

---
+++ lala.js
@@ -0,0 +1,10 @@
+var recipes = {
+  flour: "500g",
+  water: "1 cup",
+  salt: "1 tsp",
+  yeast: "2 tsp",
+  oil: "1/4 cup"
+}
+
+var newRecipes = Object.assign({}, recipes)
+console.log(newRecipes)
